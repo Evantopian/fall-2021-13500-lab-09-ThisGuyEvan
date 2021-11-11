7278508b20cdaf7d535930f82d10d0df02486fab
gnauh <evanacademics@gmail.com>
2021-11-11 10:46:34 -0500

    added tasks A-E

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
+#include "coord3D.h"
 
-// add functions here
+// Task A
+double length(Coord3D *p){
+  return sqrt(pow((*p).x, 2) + pow((*p).y, 2) + pow((*p).z, 2));
+}
+
+// Task B
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    double x = length(p1);
+    double y = length(p2);
+    return x > y ? p1 : p2;
+}
+
+// Task C
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+  (*ppos).x += (*pvel).x * dt;
+  (*ppos).y += (*pvel).y * dt;
+  (*ppos).z += (*pvel).z * dt;
+}
+
+// Task E
+Coord3D* createCoord3D(double x, double y, double z){
+  Coord3D *ppos = new Coord3D;
+  (*ppos).x = x;
+  (*ppos).y = y;
+  (*ppos).z = z;
+  return ppos;
+}
+
+
+void deleteCoord3D(Coord3D *p){
+  delete p;
+}
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
-
 #include "funcs.h"
+#include "coord3D.h"
 
-int main()
-{
+
+int main(){
+
+  Coord3D *ppos = createCoord3D(10, 20, 30);
+  Coord3D *pvel = createCoord3D(5.5, -1.4, 7.77);
+  move(ppos, pvel, 10.0);
+
+  std::cout << "Coordinates after 10 seconds: " 
+            << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << std::endl;
+
   return 0;
 }
